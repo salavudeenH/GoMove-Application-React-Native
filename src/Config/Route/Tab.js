42042f94de06264fe85fe71dd9db09a9c3e33feb
salavudeenH <salavudeenhadji@gmail.com>
2022-03-29 09:23:28 +0200

    add spec

--- src/Config/Route/Tab.js
+++ src/Config/Route/Tab.js
@@ -11,7 +11,20 @@ Icon.loadFont();
 const Routes = () => {
   const Tab = createBottomTabNavigator();
 return (
-<Tab.Navigator>
+<Tab.Navigator
+tabBarOptions={{
+  activeTintColor: '#5511E3',
+  inactiveTintColor: 'black',
+  style: {
+    borderTopColor: 'red',
+    backgroundColor: 'transparent',
+    elevation: 0,
+  },
+  labelStyle: {
+    fontSize: 10,
+  },
+}}
+>
 <Tab.Screen name="Explorer" options={{ tabBarIcon: ({size, color}) => (<Ionicons name={'git-merge-outline'} color={color} size={size} />),headerShown: false,}} component={HomeScreen} />
 <Tab.Screen name="Rechercher" options={{ tabBarIcon: ({size, color}) => (<Ionicons name={'search-outline'} color={color} size={size} />),headerShown: true,}} component={Search} />
 <Tab.Screen name="Favoris" options={{ tabBarIcon: ({size, color}) => (<Ionicons name={'heart-outline'} color={color} size={size} />),headerShown: true,}} component={Favorite} />
